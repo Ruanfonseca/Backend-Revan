91528167df510faaf1830b2184a6383aa67bd68e
Ruan Fonseca <whitelook22@outlook.com>
2025-07-19 10:52:21 -0300

    corrigindo cors

--- middlewares/corsMiddleware.js
+++ middlewares/corsMiddleware.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-  'http://localhost:8080',
-  'https://seu-site.com',
+  "http://localhost:8080",
+  "https://preview--revan-imobiliaria-web.lovable.app",
   // Adicione outras URLs permitidas aqui
 ];
 
@@ -8,15 +8,18 @@ const corsMiddleware = (req, res, next) => {
   const origin = req.headers.origin;
 
   if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader("Access-Control-Allow-Origin", origin);
   }
 
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
 
   // Responde OPTIONS diretamente
-  if (req.method === 'OPTIONS') {
+  if (req.method === "OPTIONS") {
     return res.sendStatus(204);
   }
 
