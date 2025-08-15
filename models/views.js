d938028533ad8e755e85701d54214a336056a113
Ruan Fonseca <whitelook22@outlook.com>
2025-08-14 21:53:49 -0300

    utilizando pipeline com vps

--- models/views.js
+++ models/views.js
@@ -1,20 +1,28 @@
 const sequelize = require("../config/database");
+const { DataTypes } = require("sequelize");
 
-const Views = sequelize.define({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    allowNull: false,
+const Views = sequelize.define(
+  "Views",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    qtd: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
   },
-  qtd: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  createdAt: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
-});
+  {
+    tableName: "views",
+    timestamps: false, // se não quiser createdAt/updatedAt automáticos
+  }
+);
 
 module.exports = Views;
