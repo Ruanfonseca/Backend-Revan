86862306debe08386fd35ee879ad06ea3546cbcd
Ruan Fonseca <whitelook22@outlook.com>
2025-08-15 20:41:33 -0300

    configurando e corrigindo erro

--- app.js
+++ app.js
@@ -13,8 +13,6 @@ const corsMiddleware = require("./middlewares/corsMiddleware");
 
 // Usa CORS antes de qualquer rota
 app.use(corsMiddleware);
-app.options("*", corsMiddleware);
-
 // Parse JSON
 app.use(express.json());
 
